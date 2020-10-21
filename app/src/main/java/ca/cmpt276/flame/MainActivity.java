8f31b14a8da3d58daa8267f3c9d6bf1adc964c80
Ethan <ehinchli@sfu.ca>
2020-10-21 14:14:07 -0700

    Initialize repository
    Initialize repository with an empty Android app and CI pipeline

---
+++ app/src/main/java/ca/cmpt276/flame/MainActivity.java
@@ -0,0 +1,17 @@
+package ca.cmpt276.flame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Main Activity: TODO add good description
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
