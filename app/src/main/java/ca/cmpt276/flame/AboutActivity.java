90c6ae386406e5682e2832345b581e2e0077630f
Ethan <ehinchli@sfu.ca>
2020-10-25 15:31:03 -0700

    Add toolbars
    Add basic toolbars to each activity (except MainActivity),
    containing a back button and a title

--- app/src/main/java/ca/cmpt276/flame/AboutActivity.java
+++ app/src/main/java/ca/cmpt276/flame/AboutActivity.java
@@ -1,17 +1,28 @@
 package ca.cmpt276.flame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+/**
+ * AboutActivity: TODO add proper comment once activity created
+ */
 public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.about);
+        toolbar.setNavigationOnClickListener(view -> onBackPressed());
     }
 
     protected static Intent makeIntent(Context context) {
--- app/src/main/java/ca/cmpt276/flame/ChildrenActivity.java
+++ app/src/main/java/ca/cmpt276/flame/ChildrenActivity.java
@@ -1,17 +1,28 @@
 package ca.cmpt276.flame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+/**
+ * ChildrenActivity: TODO add proper comment once activity created
+ */
 public class ChildrenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_children);
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.children);
+        toolbar.setNavigationOnClickListener(view -> onBackPressed());
     }
 
     protected static Intent makeIntent(Context context) {
