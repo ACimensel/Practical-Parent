811e916b6d23c0ed8d6b98fe53e25eab8899ea29
Ethan <ehinchli@sfu.ca>
2020-10-28 00:49:54 -0700

    Add FlipManager and FlipHistory
    Add a FlipManager singleton class and a FlipHistory class to the
    model. Also modify the ChildrenManager and Child classes to use
    UUIDs which fixes many potential bugs when state is stored using
    SharedPreferences.

--- app/src/main/java/ca/cmpt276/flame/model/Child.java
+++ app/src/main/java/ca/cmpt276/flame/model/Child.java
@@ -1,11 +1,18 @@
 package ca.cmpt276.flame.model;
 
+import java.util.UUID;
+
 /**
- * Child represents a single child. A collection of Child objects
- * are managed by the ChildrenManager class.
+ * Child represents a single child. A hash map of Child objects are managed by the
+ * ChildrenManager class. Each child is given a UUID so it can be uniquely referenced by
+ * other classes. If children are instead referred to by index (ex. in FlipHistory), then
+ * history "breaks" when a Child is deleted. If we store a Child object in FlipHistory,
+ * then we'll get multiple copies of the object when we restore state from SharedPreferences
+ * (one in ChildManager and a separate one in each FlipHistory) which breaks renaming.
  */
 public class Child {
     private String name;
+    private final UUID uuid = UUID.randomUUID();
 
     public Child(String name) {
         setName(name);
@@ -15,7 +22,11 @@ public class Child {
         return name;
     }
 
-    public void setName(String name) {
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    protected void setName(String name) {
         if(name == null || name.isEmpty()) {
             throw new IllegalArgumentException("Name must be non-empty");
         }
