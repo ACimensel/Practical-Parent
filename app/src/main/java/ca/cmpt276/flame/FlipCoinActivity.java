cb69619579c9e3a7c0cc9d3ce54781d9ed319b86
ACimensel <cimensel.a@hotmail.com>
2020-10-29 01:18:27 -0700

    [acimense] added guidelines, buttons and text to display which user goes next

--- app/src/main/java/ca/cmpt276/flame/FlipCoinActivity.java
+++ app/src/main/java/ca/cmpt276/flame/FlipCoinActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,11 +18,23 @@ import android.widget.Toast;
  */
 public class FlipCoinActivity extends AppCompatActivity {
     // TODO: MOVE THESE TO COIN FLIP MANAGER
-    private final int NUM_COIN_FRAMES = 24;
-    private final int SPINS = 4;
+    private final int SPINS = 1;
     private boolean coinSpinning = false;
     private ImageView coin;
     private Button btn;
+
+    private final static int[] frameIdArr = new int[] {
+            R.drawable.coin_frame_0, R.drawable.coin_frame_1, R.drawable.coin_frame_2,
+            R.drawable.coin_frame_3, R.drawable.coin_frame_4, R.drawable.coin_frame_5,
+            R.drawable.coin_frame_6, R.drawable.coin_frame_7, R.drawable.coin_frame_8,
+            R.drawable.coin_frame_9, R.drawable.coin_frame_10, R.drawable.coin_frame_11,
+
+            R.drawable.coin_frame_12, R.drawable.coin_frame_13, R.drawable.coin_frame_14,
+            R.drawable.coin_frame_15, R.drawable.coin_frame_16, R.drawable.coin_frame_17,
+            R.drawable.coin_frame_18, R.drawable.coin_frame_19, R.drawable.coin_frame_20,
+            R.drawable.coin_frame_21, R.drawable.coin_frame_22, R.drawable.coin_frame_23,
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,18 +64,24 @@ public class FlipCoinActivity extends AppCompatActivity {
     }
 
     private void flipCoin() {
-        for(int i = 0, revs = 0; revs < SPINS;) {
-            String frameName = "coin_frame_" + i;
-            Toast.makeText(FlipCoinActivity.this,frameName, Toast.LENGTH_SHORT).show();
+        coinSpinning = true;
 
+        Toast.makeText(FlipCoinActivity.this, "5sec elapsed!", Toast.LENGTH_SHORT).show();
+        //coin.setImageDrawable(getResources().getDrawable(R.drawable.coin_frame_12));
+
+        for(int frame = 0; frame < frameIdArr.length; frame++) {
+
+            coin.setImageDrawable(getResources().getDrawable(frameIdArr[frame]));
+
+            /*
             if (i == NUM_COIN_FRAMES - 1) {
                 i = 0;
                 revs++;
             }
-            else {
-                i++;
-            }
+            else { i++; }*/
         }
+
+        coinSpinning = false;
     }
 
     protected static Intent makeIntent(Context context) {
