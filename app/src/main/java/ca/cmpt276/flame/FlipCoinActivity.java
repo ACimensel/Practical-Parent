036d59e58249cc9e70695982b9884e0e94cd8d78
ACimensel <acimense@sfu.ca>
2020-11-05 15:32:58 -0800

    [acimense] Removed unused import

--- app/src/main/java/ca/cmpt276/flame/FlipCoinActivity.java
+++ app/src/main/java/ca/cmpt276/flame/FlipCoinActivity.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
