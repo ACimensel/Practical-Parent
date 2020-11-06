ffafb4a78c801a1f3f2529d2c5b3336e2c13d56a
Jasmeen <kJasmeen6817@gmail.com>
2020-11-06 08:22:57 -0800

    Added data for app usage and references in About activity.

--- app/src/main/java/ca/cmpt276/flame/AboutActivity.java
+++ app/src/main/java/ca/cmpt276/flame/AboutActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * AboutActivity: TODO add proper comment once activity created
@@ -17,6 +18,23 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         setupToolbar();
+        fillUsageInstruction();
+        fillReferences();
+    }
+
+    private void fillReferences() {
+        TextView references = findViewById(R.id.aboutActivity_reference_body);
+        references.setText(R.string.aboutActivity_references_data);
+    }
+
+    private void fillUsageInstruction() {
+        TextView usageDescription = findViewById(R.id.aboutActivity_coin_flip_instructions_text);
+        TextView timeoutDescription = findViewById(R.id.aboutActivity_timeout_instructions_text);
+        TextView configureChildDescription = findViewById(R.id.aboutActivity_configure_child_instructions_text);
+        usageDescription.setText(R.string.aboutActivity_coinFlipUsage);
+        timeoutDescription.setText(R.string.aboutActivity_timeoutUsage);
+        configureChildDescription.setText(R.string.aboutActivity_configureChildUsage);
+
     }
 
     private void setupToolbar() {
