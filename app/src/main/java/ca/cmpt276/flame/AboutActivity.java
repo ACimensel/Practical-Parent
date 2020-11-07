c1dc98a93a78a8c0a17d044f2a04703ee6fa10f1
ACimensel <acimense@sfu.ca>
2020-11-07 00:23:22 -0800

    [acimense] Changed some of the formatting of the About activity

--- app/src/main/java/ca/cmpt276/flame/AboutActivity.java
+++ app/src/main/java/ca/cmpt276/flame/AboutActivity.java
@@ -19,36 +19,18 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         setupToolbar();
-        fillUsageInstruction();
-        fillReferences();
-        fillAuthorDetails();
-    }
-
-    private void fillAuthorDetails() {
-        TextView authorDetails = findViewById(R.id.aboutActivity_authorBody);
-        authorDetails.setText(R.string.aboutActivity_author_details);
-    }
-
-    private void fillReferences() {
-        TextView references = findViewById(R.id.aboutActivity_reference_body);
-        references.setText(R.string.aboutActivity_references_data);
-        references.setMovementMethod(LinkMovementMethod.getInstance());
-    }
-
-    private void fillUsageInstruction() {
-        TextView usageDescription = findViewById(R.id.aboutActivity_coin_flip_instructions_text);
-        TextView timeoutDescription = findViewById(R.id.aboutActivity_timeout_instructions_text);
-        TextView configureChildDescription = findViewById(R.id.aboutActivity_configure_child_instructions_text);
-        usageDescription.setText(R.string.aboutActivity_coinFlipUsage);
-        timeoutDescription.setText(R.string.aboutActivity_timeoutUsage);
-        configureChildDescription.setText(R.string.aboutActivity_configureChildUsage);
-
+        setLinksClickable();
     }
 
     private void setupToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.about);
         toolbar.setNavigationOnClickListener(view -> onBackPressed());
+    }
+
+    private void setLinksClickable() {
+        TextView references = findViewById(R.id.aboutActivity_reference_body);
+        references.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     protected static Intent makeIntent(Context context) {
