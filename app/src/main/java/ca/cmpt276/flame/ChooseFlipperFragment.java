bd0bdd2a5227bbd7a9aaa897dc8842f00daa77c3
ACimensel <acimense@sfu.ca>
2020-11-22 02:29:22 -0800

    [acimense] Merged the recycler view adapter class into the fragment class, fixed button text sizes on activity_flip_coin.xml, replaced hard coded color values

--- app/src/main/java/ca/cmpt276/flame/ChooseFlipperFragment.java
+++ app/src/main/java/ca/cmpt276/flame/ChooseFlipperFragment.java
@@ -1,13 +1,18 @@
 package ca.cmpt276.flame;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -34,7 +39,7 @@ public class ChooseFlipperFragment extends Fragment {
 
         recyclerView = v.findViewById(R.id.chooseFlipper_recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        ChooseFlipperRecyclerViewAdapter recyclerAdapter = new ChooseFlipperRecyclerViewAdapter(getContext(), childList);
+        ChildViewAdapter recyclerAdapter = new ChildViewAdapter(getContext(), childList);
         recyclerView.setAdapter(recyclerAdapter);
 
         return v;
@@ -58,5 +63,64 @@ public class ChooseFlipperFragment extends Fragment {
             ((FlipCoinActivity) getActivity()).updateUI();
             getFragmentManager().popBackStack();
         });
+    }
+
+    private static class ChildViewAdapter extends RecyclerView.Adapter<ChildViewAdapter.ViewHolder> {
+        private final FlipManager flipManager = FlipManager.getInstance();
+        private final Context context;
+        private final List<Child> childList;
+
+        ChildViewAdapter(Context context, List<Child> childList) {
+            this.context = context;
+            this.childList = childList;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(context).inflate(R.layout.item_child, parent, false);
+            ChildViewAdapter.ViewHolder vHolder = new ChildViewAdapter.ViewHolder(v);
+
+            vHolder.childItem.setOnClickListener(v1 -> {
+                flipManager.overrideTurnChild(vHolder.childObj);
+                ((FlipCoinActivity) context).updateUI();
+                ((AppCompatActivity) context).getSupportFragmentManager().popBackStack();
+            });
+
+            return vHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder vHolder, int position) {
+            vHolder.childObj = childList.get(position);
+            vHolder.childImage.setImageBitmap(vHolder.childObj.getImageBitmap(context));
+            vHolder.childName.setText(vHolder.childObj.getName());
+            vHolder.childOrderInQ.setText(context.getString(R.string.child_pos_in_queue, position + 1));
+        }
+
+        @Override
+        public int getItemCount() {
+            return childList.size();
+        }
+
+        /**
+         * A view holder class responsible for filling the view of each item in the data set
+         */
+        private static class ViewHolder extends RecyclerView.ViewHolder {
+            private final LinearLayout childItem;
+            private final ImageView childImage;
+            private final TextView childName;
+            private final TextView childOrderInQ;
+            private Child childObj;
+
+            ViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+                childItem = itemView.findViewById(R.id.item_child);
+                childImage = itemView.findViewById(R.id.item_child_image);
+                childName = itemView.findViewById(R.id.item_child_name);
+                childOrderInQ = itemView.findViewById(R.id.item_child_order);
+            }
+        }
     }
 }
